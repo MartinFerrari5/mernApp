737392da8337b8fd097a1e8abcdf53ea2d013449
Tinxo <tinchoferrarigd@gmail.com>
2023-06-14 12:34:23 -0300

    Headers 2pt

--- client/src/context/TaskContext.jsx
+++ client/src/context/TaskContext.jsx
@@ -83,7 +83,8 @@ const TaskContextProvider = (props) =>{
             const response = await axios.get("https://node-mern-qnul.onrender.com/api",{
                 headers: {
                     "Access-Control-Allow-Origin": "*",
-                    
+                    "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+                    "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
                 }
             });
             // const result = await response.json()
